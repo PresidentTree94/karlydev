3ae3ce3bf7a7210fbd3db6864f4c02212dcfba9e
PresidentTree94 <karlysams1218@gmail.com>
2026-05-29 16:47:48 -0400

    Added sphere.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -16,7 +16,27 @@ export default async function OG() {
       backgroundColor: "#faf8f5",
       fontSize: 80,
       fontFamily: "Playfair",
+      position: "relative"
     }}>
+      <div style={{
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        pointerEvents: "none"
+      }}>
+          <div style={{
+            position: "absolute",
+            top: -128,
+            right: -128,
+            borderRadius: "100%",
+            width: 700,
+            height: 700
+          }}></div>
+          {/*<div className="absolute -bottom-40 -left-40 w-[600px] h-[600px] rounded-full bg-stone-200/50"></div>*/}
+          {/*<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[900px] h-[900px] rounded-full bg-amber-50/40"></div>*/}
+        </div>
       <span style={{ color: "#292524" }}>karly</span>
       <span style={{ color: "#fd9a00" }}>.dev</span>
     </div>,
