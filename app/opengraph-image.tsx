ba2608ff53b123aaebe186bcaac8daa04fcd4d78
PresidentTree94 <karlysams1218@gmail.com>
2026-05-29 17:00:09 -0400

    Removed sphere.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -18,24 +18,6 @@ export default async function OG() {
       fontFamily: "Playfair",
       position: "relative"
     }}>
-      <div style={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-      }}>
-          <div style={{
-            position: "absolute",
-            top: -128,
-            right: -128,
-            borderRadius: "100%",
-            width: 700,
-            height: 700,
-            backgroundColor: "#fef3c6",
-            opacity: 0.6
-          }}></div>
-        </div>
       <span style={{ color: "#292524" }}>karly</span>
       <span style={{ color: "#fd9a00" }}>.dev</span>
     </div>,
