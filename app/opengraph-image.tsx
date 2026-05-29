aca7f29149f78790476e91b07577ae2166b30aaa
PresidentTree94 <karlysams1218@gmail.com>
2026-05-29 16:05:57 -0400

    Added font to OpenGraph.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -1,8 +1,7 @@
 import { ImageResponse } from "next/og";
 
-export const size = { width: 1200, height: 630 };
-
-export default function OG() {
+export default async function OG() {
+  const playfair = await fetch(new URL("/public/PlayfairDisplay-Bold.ttf", import.meta.url)).then(res => res.arrayBuffer());
   return new ImageResponse(
     <div style={{
       width: "100%",
@@ -11,11 +10,21 @@ export default function OG() {
       justifyContent: "center",
       alignItems: "center",
       backgroundColor: "#faf8f5",
-      fontSize: "70px",
-      fontFamily: "Playfair Display"
+      fontSize: 80,
+      fontFamily: "Playfair",
     }}>
       <span style={{ color: "#292524" }}>karly</span>
       <span style={{ color: "#fd9a00" }}>.dev</span>
-    </div>, size
+    </div>,
+    {
+      width: 1200,
+      height: 630,
+      fonts: [{
+        name: "Playfair",
+        data: playfair,
+        weight: 700,
+        style: "normal"
+      }]
+    }
   );
 }
