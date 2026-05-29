bfad8bc37558acc7babfb8d17814ad7bee128b20
PresidentTree94 <karlysams1218@gmail.com>
2026-05-29 14:54:57 -0400

    Update font size.

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -10,7 +10,8 @@ export default function OG() {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
-      backgroundColor: "#faf8f5"
+      backgroundColor: "#faf8f5",
+      fontSize: "60px"
     }}>
       <span style={{ color: "#292524" }}>karly</span>
       <span style={{ color: "#fd9a00" }}>.dev</span>
